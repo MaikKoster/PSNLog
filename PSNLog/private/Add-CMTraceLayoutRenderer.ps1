b932d420feb96332b1164d5bde3ebcd1347a8619
Maik Koster <MaikKoster@users.noreply.github.com>
2018-03-21 09:56:05 -0500

    Properly support redirected messages in cmtrace layout

--- PSNLog/private/Add-CMTraceLayoutRenderer.ps1
+++ PSNLog/private/Add-CMTraceLayoutRenderer.ps1
@@ -24,31 +24,54 @@ function Add-CMTraceLayoutRenderer {
         $CMTrace = {
             param($logEvent)
 
-            # Evaluate caller information
-            $Caller = (Get-PSCallStack)[1]
-            $Component = $Caller.Command
-            $Source = $Caller.Location -replace '<No file>', ''
+            if ($null -ne $logEvent) {
+                # Evaluate caller information
+                $Callstack = Get-PSCallStack
+                if ($null -ne $Callstack) {
+                    try {
+                        if ($Callstack.count -gt 1){
+                            $Caller = $Callstack[1]
+                        } else {
+                            $Caller - $Callstack
+                        }
+                        $Component = $Caller.Command
+                        if ($Component -like 'Write-NLog*'){
+                            $Caller = $Callstack[2]
+                            $Component = $Caller.Command
+                        }
+                        $Source = $Caller.Location -replace '<No file>', ''
+                    } catch {}
+                }
 
-            switch ($logEvent.Level.ToString()) {
-                'Debug' { $Sev = 1 }
-                'Error' { $Sev = 3 }
-                'Fatal' { $Sev = 3 }
-                'Info' { $Sev = 1 }
-                'Trace' { $Sev = 1 }
-                'Warn' { $Sev = 2 }
-                'Info' { $Sev = 1 }
+                if ($null -ne $logEvent.Level) {
+                    switch ($logEvent.Level.ToString()) {
+                        'Debug' { $Sev = 1 }
+                        'Error' { $Sev = 3 }
+                        'Fatal' { $Sev = 3 }
+                        'Info' { $Sev = 1 }
+                        'Trace' { $Sev = 1 }
+                        'Warn' { $Sev = 2 }
+                        'Info' { $Sev = 1 }
+                    }
+                } else {
+                    $Sev = 1
+                }
+
+                # Get Timezone Bias to allign log entries through different timezones
+                if ($null -eq $Global:TimezoneBias) {
+                    try {
+                        [int]$Global:TimezoneBias = [System.TimeZone]::CurrentTimeZone.GetUtcOffset([datetime]::Now).TotalMinutes
+                    } catch {}
+                }
+                $Date = Get-Date -Format 'MM-dd-yyyy'
+                $Time = Get-Date -Format 'HH:mm:ss.fff'
+                $TimeString = "$Time$Global:TimezoneBias"
+
+                $Message = "<![LOG[$($logEvent.Message)]LOG]!><time=`"$TimeString`" date=`"$Date`" component=`"$Component`" context=`"`" type=`"$Sev`" thread=`"0`" file=`"$Source`">"
+                $Message
+            } else {
+                'No logEvent object supplied.'
             }
-
-            # Get Timezone Bias to allign log entries through different timezones
-            if ($null -eq $Global:TimezoneBias) {
-                    [int]$Global:TimezoneBias = [System.TimeZone]::CurrentTimeZone.GetUtcOffset([datetime]::Now).TotalMinutes
-            }
-            $Date = Get-Date -Format 'MM-dd-yyyy'
-            $Time = Get-Date -Format 'HH:mm:ss.fff'
-            $TimeString = "$Time$Global:TimezoneBias"
-
-            $Message = "<![LOG[$($logEvent.Message)]LOG]!><time=`"$TimeString`" date=`"$Date`" component=`"$Component`" context=`"`" type=`"$Sev`" thread=`"0`" file=`"$Source`">"
-            $Message
         }
 
         [NLog.LayoutRenderers.LayoutRenderer]::Register('cmtrace', [Func[NLog.LogEventInfo, object]] $CMTrace)
